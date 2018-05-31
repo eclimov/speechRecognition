194f011d399822ad428c989298376a43caba1bf5
Eduard Climov <klimovedik88@gmail.com>
2018-05-31 19:02:12 +0400

    Initial commit

---
+++ src/microphone_recognition.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+#https://habr.com/post/310622/
+#https://github.com/Uberi/speech_recognition/blob/master/examples/microphone_recognition.py
+#https://pypi.org/project/SpeechRecognition/
+
+# NOTE: this example requires PyAudio because it uses the Microphone class
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# write audio to a WAV file
+with open("microphone-results.wav", "wb") as f:
+    f.write(audio.get_wav_data())
+
+# recognize speech using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio, language="ru"))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+'''
+# recognize speech using Google Cloud Speech
+GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
+try:
+    print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
+except sr.UnknownValueError:
+    print("Google Cloud Speech could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Cloud Speech service; {0}".format(e))
+'''
